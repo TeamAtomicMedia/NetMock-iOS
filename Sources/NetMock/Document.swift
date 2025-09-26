ea23751221387a9e8d8a9dbd4d255d0ed37cbc69
Christopher Wainwright <chris.wainwright@atomicmedia.co.uk>
2025-09-26 15:53:46 +0100

    Introduce NetMock Document (#1)
    * Introduce NetMock Document (#1)
    
    * Introduce Sendable Conformance to Document (#2)

---
+++ Sources/NetMock/Document.swift
@@ -0,0 +1,55 @@
+//
+//  Document.swift
+//  NetMock
+//
+//  Created by Christopher Wainwright on 24/09/2025.
+//
+
+import Foundation
+
+extension NetMock {
+    struct Document : Sendable {
+        struct VersionNumber : Comparable, Sendable {
+            let major: Int
+            let minor: Int
+            let patch: Int?
+            
+            static func < (lhs: Self, rhs: Self) -> Bool {
+                lhs.major == rhs.major
+                    ? lhs.minor == rhs.minor
+                        ? lhs.patch ?? 0 < rhs.patch ?? 0
+                        : lhs.minor < rhs.minor
+                    : lhs.major < rhs.major
+            }
+        }
+        
+        struct Header : Equatable, Sendable  {
+            enum Method : String, CaseIterable, Sendable {
+                case GET, PUT, POST, DELETE, PATCH
+            }
+            
+            enum Identifier : Equatable, Sendable {
+                case label(String), code(Int), live
+            }
+
+            let method: Method
+            let urlString: String
+            let sequence: [Identifier]
+        }
+        
+        
+        struct Response : Sendable {
+            struct Header : Sendable {
+                let code: Int
+                let labels: [String]
+            }
+            
+            let header: Response.Header
+            let body: Data?
+        }
+        
+        let version: VersionNumber?
+        let header: Header
+        let body: [Response]
+    }
+}
