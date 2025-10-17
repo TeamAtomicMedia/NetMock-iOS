7d49eaccd8d2ca2eab7a11921978d9a4b055d323
Christopher Wainwright <chris.wainwright@atomicmedia.co.uk>
2025-10-17 17:28:37 +0100

    Clarify invalidHeaderSequence Error Name
    - Clarify naming of invalidHeaderSequence error
    - Add Doc Comments

--- Sources/NetMock/Document+Validation.swift
+++ Sources/NetMock/Document+Validation.swift
@@ -8,15 +8,18 @@
 import Foundation
 
 extension NetMock.Document {
+    /// Validation Errors to throw in case of invalid document
     enum ValidationError: Error {
-        case invalidSequence
+        case invalidHeaderSequence
         case invalidLabels([String])
     }
 
     /// Validate document format
+    /// - Prevent #Live in leading sequence items (must be final item in sequence)
+    /// - Flag Labels with '#'-prefix
     func validate() throws {
         if self.sequence.dropLast().contains(.live) {
-            throw ValidationError.invalidSequence
+            throw ValidationError.invalidHeaderSequence
         }
         
         let invalidLabels = self.sequence.compactMap {
