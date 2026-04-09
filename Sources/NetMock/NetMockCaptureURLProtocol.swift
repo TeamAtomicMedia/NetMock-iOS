89e28d5447aef986aa7e3c8c74bf0487f8ac4bc0
Jamie Froggatt <jamie.froggatt@atomicmedia.co.uk>
2026-04-09 10:19:38 +0100

    Apply safer concurrency practices

--- Sources/NetMock/NetMockCaptureURLProtocol.swift
+++ Sources/NetMock/NetMockCaptureURLProtocol.swift
@@ -8,115 +8,122 @@
 import Foundation
 
 import NetMockCore
+import Synchronization
 
 /// Apply this to the URLSessionConfiguration to send URL responses to NetMock Capture
-//public class NetMockCaptureURLProtocol: URLProtocol {
-//    static let session = URLSession(configuration: .default)
-//    
-//    public override class func canInit(with request: URLRequest) -> Bool {
-//        guard let scheme = request.url?.scheme else { return false }
-//        return scheme == "https" || scheme == "http"
-//    }
-//    
-//    override public class func canonicalRequest(for request: URLRequest) -> URLRequest { request
-//    }
-//    
-//    override public func startLoading() {
-//        let rawMethod = request.httpMethod ?? "GET"
-//        let method = Method(rawValue: rawMethod) ?? .GET
-//        let url = request.url
-//        do {
-//            let (data, urlResponse) = try objcPerformAsync { [request] in
-//                try await NetMockCaptureURLProtocol.session.data(for: request)
-//            }
-//            
-//            client?.urlProtocol(self, didReceive: urlResponse, cacheStoragePolicy: .notAllowed)
-//            client?.urlProtocol(self, didLoad: data)
-//            client?.urlProtocolDidFinishLoading(self)
-//            
-//            if let url, let httpResponse = urlResponse as? HTTPURLResponse {
-//                objcPerformAsync {
-//                    await DocumentStore.shared.add(.init(method: method, url: url, statusCode: httpResponse.statusCode, body: data))
-//                }
-//            }
-//        } catch {
-//            client?.urlProtocol(self, didFailWithError: error)
-//        }
-//    }
-//    
-//    override public func stopLoading() {}
-//    
-//    /// Applies NetMock capture globally to all URLSessions in the app
-//    ///
-//    /// This method uses method swizzling to inject NetMockCaptureURLProtocol into:
-//    /// - `URLSessionConfiguration.default`
-//    /// - `URLSessionConfiguration.ephemeral`
-//    /// - `URLSession.shared`
-//    ///
-//    /// This also applies the URLProtocol separately to WebViews and system calls
-//    ///
-//    /// After calling this method, all network responses will be captured and can be
-//    /// saved as `.nm` files using `DocumentStore.shared.save()`.
-//    /// Use the `*BypassingNetMockCapture` variants to create
-//    /// URLSessions that should not be captured.
-//    ///
-//    /// Example:
-//    /// ```swift
-//    /// NetMockCaptureURLProtocol.applyGlobally()
-//    /// // Now all network responses will be captured
-//    /// // Later, save them:
-//    /// await DocumentStore.shared.save()
-//    /// ```
-//    @MainActor
-//    public static func applyGlobally() {
-//        _ = NetMockCaptureURLProtocol.session
-//        
-//        // WebViews and system
-//        URLProtocol.registerClass(Self.self)
-//        
-//        // Apply to all URLSessions created in project
-//        let configDefaultOriginal = class_getClassMethod(URLSessionConfiguration.self, #selector(getter: URLSessionConfiguration.defaultBypassingNetMockCapture))!
-//        let configDefaultReplacement = class_getClassMethod(URLSessionConfiguration.self, #selector(getter: URLSessionConfiguration.`default`))!
-//        method_exchangeImplementations(configDefaultOriginal, configDefaultReplacement)
-//        
-//        let configEphemeralOriginal = class_getClassMethod(URLSessionConfiguration.self, #selector(getter: URLSessionConfiguration.ephemeralBypassingNetMockCapture))!
-//        let configEphemeralReplacement = class_getClassMethod(URLSessionConfiguration.self, #selector(getter: URLSessionConfiguration.ephemeral))!
-//        method_exchangeImplementations(configEphemeralOriginal, configEphemeralReplacement)
-//        
-//        let sessionSharedOriginal = class_getClassMethod(URLSession.self, #selector(getter: URLSession.sharedBypassingNetMockCapture))!
-//        let sessionSharedReplacement = class_getClassMethod(URLSession.self, #selector(getter: URLSession.shared))!
-//        method_exchangeImplementations(sessionSharedOriginal, sessionSharedReplacement)
-//    }
-//}
-//
-//extension URLSessionConfiguration {
-//    /// Use this method to get `URLSessionConfiguration.default` without NetMock applied.
-//    /// This method must be called after `NetMockCaptureURLProtocol.applyGlobally()`.
-//    @objc public class var defaultBypassingNetMockCapture: URLSessionConfiguration {
-//        // The implementation to be used post-swizzle.
-//        let config = URLSessionConfiguration.defaultBypassingNetMockCapture // Now has .default's implementation. Pre-swizzle, this will infinitely recurse and stack overflow.
-//        config.protocolClasses = [NetMockCaptureURLProtocol.self] + (config.protocolClasses ?? [])
-//        return config
-//    }
-//    /// Use this method to get `URLSessionConfiguration.ephemeral` without NetMock applied.
-//    /// This method must be called after `NetMockCaptureURLProtocol.applyGlobally()`.
-//    @objc public class var ephemeralBypassingNetMockCapture: URLSessionConfiguration {
-//        // The implementation to be used post-swizzle.
-//        let config = URLSessionConfiguration.ephemeralBypassingNetMockCapture // Now has .ephemeral's implementation. Pre-swizzle, this will infinitely recurse and stack overflow.
-//        config.protocolClasses = [NetMockCaptureURLProtocol.self] + (config.protocolClasses ?? [])
-//        return config
-//    }
-//}
-//
-//extension URLSession {
-//    /// Use this method to get `URLSession.shared` without NetMock applied.
-//    /// This method must be called after `NetMockCaptureURLProtocol.applyGlobally()`.
-//    @objc public class var sharedBypassingNetMockCapture: URLSession {
-//        // The implementation to be used post-swizzle.
-//        let session = URLSession.sharedBypassingNetMockCapture // Now has .shared's implementation. Pre-swizzle, this will infinitely recurse and stack overflow.
-//        if session.configuration.protocolClasses?.contains(where: { $0 == NetMockCaptureURLProtocol.self }) == false {
-//            return URLSession(configuration: .default)
-//        }
-//        return session
-//    }
-//}
+public class NetMockCaptureURLProtocol: URLProtocol, @unchecked Sendable {
+    static let session = URLSession(configuration: .default)
+    private let fetchTask: Mutex<Task<(), Never>?> = .init(nil)
+    
+    public override class func canInit(with request: URLRequest) -> Bool {
+        guard let scheme = request.url?.scheme else { return false }
+        return scheme == "https" || scheme == "http"
+    }
+    
+    override public class func canonicalRequest(for request: URLRequest) -> URLRequest { request
+    }
+    
+    override public func startLoading() {
+        let rawMethod = request.httpMethod ?? "GET"
+        let method = Method(rawValue: rawMethod) ?? .GET
+        let url = request.url
+        fetchTask.withLock { fetchTask in
+            fetchTask = Task { @Sendable [client, request] in
+                do {
+                    let (data, urlResponse) = try await NetMockCaptureURLProtocol.session.data(for: request)
+                    
+                    client?.urlProtocol(self, didReceive: urlResponse, cacheStoragePolicy: .notAllowed)
+                    client?.urlProtocol(self, didLoad: data)
+                    client?.urlProtocolDidFinishLoading(self)
+                    
+                    if let url, let httpResponse = urlResponse as? HTTPURLResponse {
+                        await DocumentStore.shared.add(.init(method: method, url: url, statusCode: httpResponse.statusCode, body: data))
+                    }
+                } catch {
+                    client?.urlProtocol(self, didFailWithError: error)
+                }
+            }
+        }
+    }
+    
+    override public func stopLoading() {
+        fetchTask.withLock { fetchTask in
+            fetchTask?.cancel()
+            fetchTask = nil
+        }
+    }
+    
+    /// Applies NetMock capture globally to all URLSessions in the app
+    ///
+    /// This method uses method swizzling to inject NetMockCaptureURLProtocol into:
+    /// - `URLSessionConfiguration.default`
+    /// - `URLSessionConfiguration.ephemeral`
+    /// - `URLSession.shared`
+    ///
+    /// This also applies the URLProtocol separately to WebViews and system calls
+    ///
+    /// After calling this method, all network responses will be captured and can be
+    /// saved as `.nm` files using `DocumentStore.shared.save()`.
+    /// Use the `*BypassingNetMockCapture` variants to create
+    /// URLSessions that should not be captured.
+    ///
+    /// Example:
+    /// ```swift
+    /// NetMockCaptureURLProtocol.applyGlobally()
+    /// // Now all network responses will be captured
+    /// // Later, save them:
+    /// await DocumentStore.shared.save()
+    /// ```
+    @MainActor
+    public static func applyGlobally() {
+        _ = NetMockCaptureURLProtocol.session
+        
+        // WebViews and system
+        URLProtocol.registerClass(Self.self)
+        
+        // Apply to all URLSessions created in project
+        let configDefaultOriginal = class_getClassMethod(URLSessionConfiguration.self, #selector(getter: URLSessionConfiguration.defaultBypassingNetMockCapture))!
+        let configDefaultReplacement = class_getClassMethod(URLSessionConfiguration.self, #selector(getter: URLSessionConfiguration.`default`))!
+        method_exchangeImplementations(configDefaultOriginal, configDefaultReplacement)
+        
+        let configEphemeralOriginal = class_getClassMethod(URLSessionConfiguration.self, #selector(getter: URLSessionConfiguration.ephemeralBypassingNetMockCapture))!
+        let configEphemeralReplacement = class_getClassMethod(URLSessionConfiguration.self, #selector(getter: URLSessionConfiguration.ephemeral))!
+        method_exchangeImplementations(configEphemeralOriginal, configEphemeralReplacement)
+        
+        let sessionSharedOriginal = class_getClassMethod(URLSession.self, #selector(getter: URLSession.sharedBypassingNetMockCapture))!
+        let sessionSharedReplacement = class_getClassMethod(URLSession.self, #selector(getter: URLSession.shared))!
+        method_exchangeImplementations(sessionSharedOriginal, sessionSharedReplacement)
+    }
+}
+
+extension URLSessionConfiguration {
+    /// Use this method to get `URLSessionConfiguration.default` without NetMock applied.
+    /// This method must be called after `NetMockCaptureURLProtocol.applyGlobally()`.
+    @objc public class var defaultBypassingNetMockCapture: URLSessionConfiguration {
+        // The implementation to be used post-swizzle.
+        let config = URLSessionConfiguration.defaultBypassingNetMockCapture // Now has .default's implementation. Pre-swizzle, this will infinitely recurse and stack overflow.
+        config.protocolClasses = [NetMockCaptureURLProtocol.self] + (config.protocolClasses ?? [])
+        return config
+    }
+    /// Use this method to get `URLSessionConfiguration.ephemeral` without NetMock applied.
+    /// This method must be called after `NetMockCaptureURLProtocol.applyGlobally()`.
+    @objc public class var ephemeralBypassingNetMockCapture: URLSessionConfiguration {
+        // The implementation to be used post-swizzle.
+        let config = URLSessionConfiguration.ephemeralBypassingNetMockCapture // Now has .ephemeral's implementation. Pre-swizzle, this will infinitely recurse and stack overflow.
+        config.protocolClasses = [NetMockCaptureURLProtocol.self] + (config.protocolClasses ?? [])
+        return config
+    }
+}
+
+extension URLSession {
+    /// Use this method to get `URLSession.shared` without NetMock applied.
+    /// This method must be called after `NetMockCaptureURLProtocol.applyGlobally()`.
+    @objc public class var sharedBypassingNetMockCapture: URLSession {
+        // The implementation to be used post-swizzle.
+        let session = URLSession.sharedBypassingNetMockCapture // Now has .shared's implementation. Pre-swizzle, this will infinitely recurse and stack overflow.
+        if session.configuration.protocolClasses?.contains(where: { $0 == NetMockCaptureURLProtocol.self }) == false {
+            return URLSession(configuration: .default)
+        }
+        return session
+    }
+}
